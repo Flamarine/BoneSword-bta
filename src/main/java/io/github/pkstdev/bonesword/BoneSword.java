f89855c6b1fb1c24deb9a98744f02de0eb54830f
pkstDev <William27060827@hotmail.com>
2022-06-30 21:20:40 +0800

    Recipe!
    Bump version to 1.0.1

--- src/main/java/io/github/pkstdev/bonesword/BoneSword.java
+++ src/main/java/io/github/pkstdev/bonesword/BoneSword.java
@@ -2,7 +2,7 @@ package io.github.pkstdev.bonesword;
 
 import bta.Mod;
 import net.minecraft.client.Minecraft;
-import net.minecraft.src.Item;
+import net.minecraft.src.*;
 
 public class BoneSword implements Mod {
     public static Item BONE_SWORD;
@@ -10,6 +10,7 @@ public class BoneSword implements Mod {
     @Override
     public void init(Minecraft minecraft) {
         BONE_SWORD = new ItemBoneSword(135, 6).setIconCoord(1, 13).setItemName("bonesword");
+        RecipeHelper.addRecipe(new ItemStack(BONE_SWORD), "BDB", "BDB", " S ", 'D', Item.ingotSteelCrude, 'B', Item.bone, 'S', Item.stick);
         System.out.println("[BoneSword] Initialized.");
     }
 
